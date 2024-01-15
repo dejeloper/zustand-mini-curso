092d7fd2b517a180722dca1e43feec863b0f8b88
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-01-14 21:42:58 -0500

    Refactor JiraPage component to use task store

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -1,6 +1,15 @@
-import { JiraTasks } from '../../components';
+import { JiraTasks } from "../../components";
+import { useTaskStore } from "../../stores";
 
 export const JiraPage = () => {
+  const pendingTasks = useTaskStore((state) => state.getTasksByStatus("open"));
+
+  const inProgressTasks = useTaskStore((state) =>
+    state.getTasksByStatus("in-progress")
+  );
+
+  const doneTasks = useTaskStore((state) => state.getTasksByStatus("done"));
+
   return (
     <>
       <h1>Tareas</h1>
@@ -8,19 +17,16 @@ export const JiraPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          
-          <JiraTasks title='Pendientes' value='pending' />
-          
-          <JiraTasks title='Avanzando' value='in-progress' />
-          
-          <JiraTasks title='Terminadas' value='done' />
+        <JiraTasks title="Pendientes" value="open" task={pendingTasks} />
 
+        <JiraTasks
+          title="Avanzando"
+          value="in-progress"
+          task={inProgressTasks}
+        />
+
+        <JiraTasks title="Terminadas" value="done" task={doneTasks} />
       </div>
-
-      
-
-
-
     </>
   );
 };
